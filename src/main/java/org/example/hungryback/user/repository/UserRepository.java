87c83e516fea0c6511f03a21e61f0162183e9e27
btongtong <leeym2615@naver.com>
2024-04-25 22:25:15 +0900

    error files delete

--- src/main/java/org/example/hungryback/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.hungryback.user.repository;
-
-import org.example.hungryback.user.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByEmail(String email);
-    UserEntity findByNickname(String nickname);
-}
